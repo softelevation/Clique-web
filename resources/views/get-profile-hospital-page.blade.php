c84a28785ef81a3fa09cfb339038513145c78d03
vijay sandhu <vijay.pro2020@gmail.com>
2021-09-13 18:39:46 +0530

    icone flag hospital implement

--- resources/views/get-profile-hospital-page.blade.php
+++ resources/views/get-profile-hospital-page.blade.php
@@ -3,7 +3,7 @@
 <!--begin::Head-->
     <head>
         <meta charset="utf-8"/>
-        <title>Clique | Login</title>
+        <title>Clique | Hospital</title>
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="description" content="Login page example"/>
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no"/>
