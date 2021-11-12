3793e4f5661563ca428dacb1b374fdd7f662cf7d
vijay sandhu <vijay.pro2020@gmail.com>
2021-11-12 12:54:40 +0530

    fade out imaplement social icone active

--- app/Http/Controllers/Api/Auth/LoginController.php
+++ app/Http/Controllers/Api/Auth/LoginController.php
@@ -1777,18 +1777,13 @@ class LoginController extends Controller
 		try{
 			$input = JWTAuth::toUser();
 			$profileIcone = ProfileIcone::find($request->id);
-			if($profileIcone->fade_out){
-				ProfileIcone::where('id','!=',$request->id)->where('profile_id',$profileIcone->profile_id)->update(array('fade_out'=>0));
-			}else{
-				ProfileIcone::where('id',$request->id)->update(array('fade_out'=>1));
-			}
 			
-			// $ProfileIconeCheck = ProfileIcone::where('profile_id',$profileIcone->profile_id)->where('fade_out',0)->first();
-			// if($ProfileIconeCheck){
-				// ProfileIcone::where('profile_id',$profileIcone->profile_id)->update(array('fade_out'=>1));
-			// }else{
-				// 
-			// }
+			$ProfileIconeCheck = ProfileIcone::where('profile_id',$profileIcone->profile_id)->where('fade_out',0)->first();
+			if($ProfileIconeCheck){
+				ProfileIcone::where('profile_id',$profileIcone->profile_id)->update(array('fade_out'=>1));
+			}else{
+				ProfileIcone::where('id','!=',$request->id)->where('profile_id',$profileIcone->profile_id)->update(array('fade_out'=>0));
+			}
 			return $this->sendResult($message,$data,$errors,$status);
 		}catch(\Exception $e){
 			$status = false;
